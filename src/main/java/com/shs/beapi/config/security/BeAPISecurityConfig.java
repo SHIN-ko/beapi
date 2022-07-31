175dff69565afda4da5c7f5fe09bfde72e7edfa4
SHIN <shs2810@gmail.com>
2022-07-31 11:25:42 +0900

    security apply

---
+++ src/main/java/com/shs/beapi/config/security/BeAPISecurityConfig.java
@@ -0,0 +1,22 @@
+package com.shs.beapi.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BeAPISecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .antMatchers("/resources/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().build();
+    }
+}
+
