ad0cd86103a7c842cb51734a611b0a5a2edaf6fe
SHIN <shs2810@gmail.com>
2022-07-31 09:52:35 +0900

    init setting

---
+++ src/main/java/com/shs/beapi/BeapiApplication.java
@@ -0,0 +1,13 @@
+package com.shs.beapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeapiApplication.class, args);
+    }
+
+}
